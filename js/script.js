d4ada3b2359ea88e10a8d85d61d75dc544db9fff
Bolat <abilaev@mail.ru>
2019-01-12 10:18:12 +0600

    Braun new version

---
+++ js/script.js
@@ -0,0 +1,10 @@
+var menuBlock = document.getElementById('mobileMenuBlock');
+
+function openMenu() {
+
+	menuBlock.style.display = 'block';
+}
+
+function closeMenu() {
+	menuBlock.style.display = 'none';
+}
